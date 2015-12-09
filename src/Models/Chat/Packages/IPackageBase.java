eefc94d005f9040e265c5cdc5ff68a1e48ed64cb
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-12-09 12:00:36 +0100

    added a package base interface, a MessagePackage, and an enum to keep track of which package it is

---
+++ src/Models/Chat/Packages/IPackageBase.java
@@ -0,0 +1,8 @@
+package Models.Chat.Packages;
+
+/**
+ * Created by JesperB on 09-12-2015.
+ */
+public interface IPackageBase {
+    public PackageType getPackageType();
+}
---
+++ src/Models/Chat/Packages/PackageType.java
@@ -0,0 +1,12 @@
+package Models.Chat.Packages;
+
+/**
+ * Created by JesperB on 09-12-2015.
+ */
+public enum PackageType {
+    MessagePackage,
+    LoginPackage,
+    LogoutPackage,
+    ServerStop,
+    RequestUserList
+}

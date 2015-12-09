bd65a55ec674c8bd0e05802d12ccab49595eb9b4
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-12-09 13:38:54 +0100

    error in last commit... somehow the ide didn't want to include the controller

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -1,22 +1,54 @@
 package Controllers;
 
-import Controllers.CustomControls.Chat.ChatChannelController;
+import Controllers.Chat.ChatChannelController;
 import Controllers.Modals.PrefController;
+import Models.Chat.Packages.MessagePackage;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.TitledPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable{
+    public TextArea ChatTextArea;
+    public TextField ChatMessageArea;
+    public Button ChatSendButton;
+
     ControllerMediator mediator = ControllerMediator.getInstance();
 
     public Controller(){
         mediator.mainController = this;
     }
 
-    //region actions that buttons are mapped to
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ChatSendButton.setOnAction(event -> {
+            //todo send message via client service...
+            // maybe, or just bypass that step on server and send directly on server socket
+            // haven't quite figured out how this'll work yet
+
+            //this is redundant... when the server receives the msgPackage it will be sent to all clients
+            //including this one, if we add the msg when we receive the msgPackage again we can confirm that
+            //it was actually sent to the server
+            addMessage(new MessagePackage("Server", Date.from(Instant.now()), ChatMessageArea.getText()));
+            ChatMessageArea.clear();
+        });
+    }
+
+    public void addMessage(MessagePackage msgPackage){
+        ChatTextArea.appendText(
+                msgPackage.getAuthor()
+                        .concat(": ")
+                        .concat(msgPackage.getMessage())
+                        .concat("\n"));
+    }
+
+    //region menu actions
 
     //region channels actions
 
@@ -46,6 +78,7 @@ public class Controller {
         System.out.println("graceful exit activated");
         System.exit(0);
     }
+
 
     //endregion
 }
